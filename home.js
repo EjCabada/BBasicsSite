6810f05a45b33162ae33edf23225225f492e947e
EJ Cabada <cabada.eric7@gmail.com>
2023-04-25 21:23:32 -0700

    Add files via upload

--- home.js
+++ home.js
@@ -1,21 +1,34 @@
 // import '@cxeweb/pattern-library/lib/components/heading';
 // import '@cxeweb/pattern-library/lib/components/text-styling-examples';
 
-let expirationQuickBox = document.querySelector('#expirationQuickBox');
+let quickContent = document.querySelectorAll('.quickContent');
 let expirationBox = document.querySelector('#expirationDates');
-let csPageLink = document.querySelector('#csPageLink');
-let returnButton = document.querySelector('.returnButton');
+let prepBox = document.querySelector('#prep');
+let barHelpBox = document.querySelector('#barHelp');
+let returnButtons = document.querySelectorAll('.returnButton');
 
-
-expirationQuickBox.addEventListener('click', function() {
+quickContent[0].addEventListener('click', function() {
     expirationBox.classList.remove('hidden');
     expirationBox.classList.add('visible');
 });
 
-returnButton.addEventListener('click', function() {
-    expirationBox.classList.remove('visible');
-    expirationBox.classList.add('hidden');
-})
+quickContent[1].addEventListener('click', function() {
+    prepBox.classList.remove('hidden');
+    prepBox.classList.add('visible');
+});
+
+quickContent[2].addEventListener('click', function() {
+    barHelpBox.classList.remove('hidden');
+    barHelpBox.classList.add('visible');
+});
+
+for (let i = 0; i < returnButtons.length; i++) {
+    returnButtons[i].addEventListener('click', function() {
+        let visibleBox = document.querySelector('.quickLinkBackground.visible');
+        visibleBox.classList.remove('visible');
+        visibleBox.classList.add('hidden');
+    });
+}
 
 // csPageLink.addEventListener('click' , function() {
 //     window.location.href = 'CS/CS.html';
